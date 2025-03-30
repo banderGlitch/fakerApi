cf6debb32826fd1267a3bdfee1bd57056016fb3a
banderGlitch <nernaykumar98@gmail.com>
2025-03-30 15:13:27 +0530

    debouncing

--- app/components/ProductList.jsx
+++ app/components/ProductList.jsx
@@ -6,17 +6,28 @@ import { useInView } from 'react-intersection-observer';
 import ProductSkeleton from './ProductSkeleton';
 import ProductCard from './ProductCard';
 import Loader from './loader';
+import axios from 'axios';
+import _, { values } from 'lodash';
+// implementing debouncing
+
 
 export default function ProductList() {
     const [page, setPage] = useState(1);
     const [mode, setMode] = useState('infinite')
     const [allProducts, setAllProducts] = useState([]);
     const { ref, inView } = useInView({ threshold: 1 });
-    const bottomRef = useRef(null);
+
+    // Search Result
+    const [search, setSearch] = useState('');
+    const [debouncedSearch, setDebouncedSearch] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const [isSearching, setIsSearching] = useState(false);
 
     const { data, isLoading, isError, isFetching } = useProducts(page);
     console.log("Data", data)
 
+    // check it has more data
+    // const hasMore = data.total ? allProducts.length > data.total : true
 
     useEffect(() => {
         if (mode == "infinite" && data?.products) {
@@ -33,10 +44,44 @@ export default function ProductList() {
 
     }, [inView, isFetching, mode])
 
+    // Implement the debouncing here 
+    // React won't re-render when that values changes
+
+    const handleSearch = useRef(
+        _.debounce(async (value) => {
+            console.log("value----->", value)
+            // if (!value.trim()) {
+            //     setSearchResults([])
+            //     setIsSearching(false);
+            //     return
+            // }
+
+            try {
+                const res = await axios.get(`https://dummyjson.com/products/search?q=${value}`);
+                console.log("res from debouncing-->", res)
+                setSearchResults(res.data.products || []);
+                setIsSearching(true);
+            } catch (err) {
+                console.error("Search error", err);
+            }
+
+        }, 500)
+
+    ).current
+
+
+    useEffect(() => {
+        console.log("searchResult", searchResults)
+    },[searchResults])
+
+
 
 
     return (
         <>
+        <div className='d-flex justify-content-center m-3'>
+            <input onChange={(e) => handleSearch(e.target.value)} className='w-full' placeholder='Search here'/>
+        </div>
             {isLoading ? (
                 <div className='row'>
                     {Array.from({ length: 8 }).map((_, i) => (
@@ -44,7 +89,7 @@ export default function ProductList() {
                             <ProductSkeleton key={i} />
                         </div>
                     ))}
-                  <Loader/>
+                    <Loader />
                 </div>
             ) : isError ? (
                 <p className="text-danger">Error loading products</p>
